e25aa315fc5ff0b1be45d0f757c8e875869a9984
Prasanna Selvaraj <prasy.loyola@gmail.com>
2022-08-11 01:26:18 +0800

    add webdriver wait

--- src/main/java/com/seleniumauto/pages/LoginPage.java
+++ src/main/java/com/seleniumauto/pages/LoginPage.java
@@ -3,6 +3,11 @@ package com.seleniumauto.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.bidi.log.Log;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPage {
 
@@ -12,13 +17,17 @@ public class LoginPage {
     static final String errorMessageLabel =  "//form[@class='login-form']//div[@class='alert alert-danger']";
 
     WebDriver driver;
+    WebDriverWait wait;
     public LoginPage(WebDriver driver){
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        this.wait.pollingEvery(Duration.ofMillis(200));
     }
 
     public void login(String username, String password){
 
         driver.get("https://phptravels.org/login");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(emailInput)));
         driver.findElement(By.xpath(emailInput)).sendKeys(username);
         driver.findElement(By.xpath(passwordInput)).sendKeys(password);
         driver.findElement(By.xpath(loginButton)).click();
